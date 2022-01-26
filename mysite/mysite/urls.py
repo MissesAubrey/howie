ae35b9c057fed028a3c104bb39f9abaf6305720e
Sioan Zohar <zohar.sioan@gmail.com>
2022-01-25 21:51:55 -0500

    DOC: updating quick start guide
    DOC: no
    
    DEV: testing

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,6 +20,7 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('chat/', include('chat.urls')),
+    path('testing/', TemplateView.as_view(template_name='testing/testing.html'), name='home'),
     path('admin/', admin.site.urls),
     path('accounts/',include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
