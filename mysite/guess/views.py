9a29a33117583a1e36c0eba3c22f65e1ef0f331a
Sioan Zohar <zohar.sioan@gmail.com>
2022-01-28 05:58:49 -0500

    MAINT: cleaning up login stuff to centralized location

--- mysite/guess/views.py
+++ mysite/guess/views.py
@@ -20,20 +20,3 @@ def feeling_need_guesser_inst(message):
     my_guesser = feeling_need_guesser()
     result_message = my_guesser.get_feelings(message)
     return result_message
-
-@login_required
-def change_password(request):
-    if request.method == 'POST':
-        form = PasswordChangeForm(request.user, request.POST)
-        if form.is_valid():
-            user = form.save()
-            update_session_auth_hash(request, user)  # Important!
-            messages.success(request, 'Your password was successfully updated!')
-            return redirect('change_password')
-        else:
-            messages.error(request, 'Please correct the error below.')
-    else:
-        form = PasswordChangeForm(request.user)
-    return render(request, 'accounts/change_password.html', {
-        'form': form
-    })
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'channels',
     'chat',
     'guess',
+    'loginapps',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
