9259212ff43f7bca8f47bc484a1d27fa666b0cb1
Sioan Zohar <zohar.sioan@gmail.com>
2022-02-01 14:41:50 -0500

    DEV: randomizing secret key for production

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_t9l97utsfc6ydv9c4ei7(=*60&t1tcbii%z7u7pe@fvt410b&'
+#SECRET_KEY = 'django-insecure-_t9l97utsfc6ydv9c4ei7(=*60&t1tcbii%z7u7pe@fvt410b&'
+SECRET_KEY=get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
