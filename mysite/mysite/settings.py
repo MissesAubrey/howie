d56c044bdb28f921ce545b7f5ce2b87202ccdaa1
Sioan Zohar <zohar.sioan@gmail.com>
2022-02-01 20:15:35 -0500

    DEV: removing secret key from view

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-_t9l97utsfc6ydv9c4ei7(=*60&t1tcbii%z7u7pe@fvt410b&'
 SECRET_KEY=get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
