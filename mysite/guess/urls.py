c711626c2b48ca00aa8dd050fbb7429632253df6
Sioan Zohar <zohar.sioan@gmail.com>
2022-02-02 15:51:44 -0500

    DEV: manually exposing css file.  Crdue hack

--- mysite/guess/urls.py
+++ mysite/guess/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.guess, name='guess'),
     path('guess', views.my_guesser, name='my_guess'),
     path('hello', views.hello_world, name='hello_world'),
+    path('static/guess/', views.my_guess_style, name='my_guess_style'),
 ]
